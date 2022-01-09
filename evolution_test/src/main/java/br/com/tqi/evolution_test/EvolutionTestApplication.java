472a78621c7c7439266376b8f6f29f228491c541
dianderson <dianderson.mendonca@gmail.com>
2022-01-09 16:02:25 -0300

    Project spring structure

---
+++ evolution_test/src/main/java/br/com/tqi/evolution_test/EvolutionTestApplication.java
@@ -0,0 +1,13 @@
+package br.com.tqi.evolution_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolutionTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolutionTestApplication.class, args);
+	}
+
+}
---
+++ evolution_test/src/test/java/br/com/tqi/evolution_test/EvolutionTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tqi.evolution_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvolutionTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
